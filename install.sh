7f69ae06436aa192ba4636042234bcfc96834b08
hjalmare <maileye@gmail.com>
2016-11-12 12:07:11 +0100

    remove space to test fugtive

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt-get install rust
 #I3
 stow -t ~ -d ~/.dotfiles/apps/ i3
 
-#Vim 
+#Vim
 stow --no-folding -t ~ -d ~/.dotfiles/apps/ vim
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
